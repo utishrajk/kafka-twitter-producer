4881d20dc6d6321ff24b6d57a180d985fef0b136
utish <utishrajk@gmail.com>
2019-03-17 11:08:12 -0400

    Use createdAt

--- src/main/java/com/producer/model/Tweet.java
+++ src/main/java/com/producer/model/Tweet.java
@@ -1,85 +1,19 @@
 package com.producer.model;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Tweet {
+
     private long id;
     private String text;
     private String lang;
     private User user;
 
-    @SerializedName("retweet_count")
-    private int retweetCount;
+    @SerializedName("created_at")
+    private String createdAt;
 
-    @SerializedName("favorite_count")
-    private int favoriteCount;
-
-    public Tweet(long id, String text, String lang, User user, int retweetCount, int favoriteCount) {
-        this.id = id;
-        this.text = text;
-        this.lang = lang;
-        this.user = user;
-        this.retweetCount = retweetCount;
-        this.favoriteCount = favoriteCount;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getLang() {
-        return lang;
-    }
-
-    public void setLang(String lang) {
-        this.lang = lang;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public int getRetweetCount() {
-        return retweetCount;
-    }
-
-    public void setRetweetCount(int retweetCount) {
-        this.retweetCount = retweetCount;
-    }
-
-    public int getFavoriteCount() {
-        return favoriteCount;
-    }
-
-    public void setFavoriteCount(int favoriteCount) {
-        this.favoriteCount = favoriteCount;
-    }
-
-    @Override
-    public String toString() {
-        return "Tweet{" +
-                "id=" + id +
-                ", text='" + text + '\'' +
-                ", lang='" + lang + '\'' +
-                ", user=" + user +
-                ", retweetCount=" + retweetCount +
-                ", favoriteCount=" + favoriteCount +
-                '}';
-    }
 }
